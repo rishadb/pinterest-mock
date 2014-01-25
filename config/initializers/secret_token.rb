cbf886162c0576cd0f7e7d613abeb9244e5830aa
Rishad Bharucha <rb2102@gmail.com>
2014-01-25 13:38:50 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PinterestMock::Application.config.secret_key_base = '0214f579f7cf273ecc4374bfab6bfbf113c3b8a69bd74335f9d2e85a73cbe96a61a6a8641c523ac1fb77db9a2f37fb73cb4caa93a9d9190aa92ff04a4ebdb536'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PinterestMock::Application.config.session_store :cookie_store, key: '_pinterest-mock_session'
