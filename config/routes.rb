312e398fb3ffbf2752deb14aea8d4d9e7587354b
Rishad Bharucha <rb2102@gmail.com>
2014-01-25 14:23:52 -0700

    Add Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 PinterestMock::Application.routes.draw do
+  root :to => 'pages#home'
+  get 'about' => 'pages#about'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
