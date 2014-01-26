bc39625ca578572bd5330d4cc58f464f16f11232
Rishad Bharucha <rb2102@gmail.com>
2014-01-26 10:15:52 -0700

    Install Devise and tweak Bootstrap navigation

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,8 @@ module PinterestMock
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    
+    # Required for Rails 3.2 deployment of Devise on Heroku
+    # config.assets.initialize_on_precompile = false
   end
 end
