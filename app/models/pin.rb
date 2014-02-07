41dc41b88d87643d9b660a020faf462b36c771d8
Rishad Bharucha <rb2102@gmail.com>
2014-02-07 09:21:29 -0700

    Revert to Add Pins Associations

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,6 @@
 class Pin < ActiveRecord::Base
 	validates :description, presence:true
+<<<<<<< HEAD
 	validates :user_id, presence:true
 	validates_attachment :image, presence:true, 
 															 content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif']}, 
@@ -7,4 +8,6 @@ class Pin < ActiveRecord::Base
 
 	belongs_to :user
 	has_attached_file :image, styles: { medium: "320x240>"}
+=======
+>>>>>>> parent of 540d715... Add Pins Associations
 end
